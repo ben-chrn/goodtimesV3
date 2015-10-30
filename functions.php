9894763e12ea954eab62f4bd1dbe6b1a317d73d9
Benjamin Chareyron <BenChrn@MacBook-Pro-de-Benjamin-3.local>
2015-10-30 23:55:39 +0100

    start dev back

--- functions.php
+++ functions.php
@@ -1,5 +1,42 @@
 <?php
 
-add_filter( 'get_search_form');
+add_action('init','create_post_type');
+function create_post_type(){
+  register_post_type('recette',
+    array(
+      'labels'=>array(
+        'name' => __('Recettes'),
+        'singular_name' => __('Recette')
+        ),
+      'public' => true,
+      'supports' => array('title','editor','thumbnail'),
+      'has_archive' => true
+      )
+    );
+
+  register_post_type('ingrédient',
+    array(
+      'labels'=>array(
+        'name' => __('Ingrédients'),
+        'singular_name' => __('Ingrédient')
+        ),
+      'public' => true,
+      'supports' => array('title'),
+      )
+    );
+
+  register_taxonomy(
+    'type',
+    'recette',
+    array(
+      'label' => __('Type'),
+      'hierarchical' => true,
+    )
+  );
+};
+
+add_theme_support('post-thumbnails');
+add_image_size('img_liste', 270, 220, array('center','top')); 
+add_image_size('home', 370, 220, true); 
 
 ?>
--- page-proposer-une-recette.php
+++ page-proposer-une-recette.php
@@ -1,3 +1,5 @@
+<?php acf_form_head(); ?>
+
 <?php get_header(); ?>
 
 <section class="bg-img"> 
@@ -6,7 +8,7 @@
     <div class="container">
         <section class="form_proposer">
             <h1>Proposez nous votre recette</h1>
-            <form>
+            <!-- <form>
                 <div class="title"><input type="text" class="text_base" placeholder="Titre de votre recette" class="col-xs-12"></div>
                 <div class="title"><input type="text" class="text_base" placeholder="Titre de votre recette" class="col-xs-12"></div>
                 <div class="type col-xs-12 col-md-3">
@@ -22,7 +24,20 @@
                 <div class="prix">
                     <input type="number" placeholder="Prix /5" min="0" max="5">
                 </div>           
-            </form>
+            </form> -->
+          <?php acf_form(array(
+              'id' => 'contact-form',
+              'post_id' => 'new_post',
+              'new_post' => array(
+                  'post_type' => 'recette',
+                  'post_status' => 'draft',
+              ),
+              'post_title' => 'true',
+              'submit_value' => 'Proposez !',
+              'return' => get_bloginfo('url') .'/merci',
+              ));
+          ?>
+
         </section>
         <div class="clear"></div>
     </div>
