c2f77e561e9823189cb788f64a4dfae8038fc22e
Benjamin Chareyron <BenChrn@MacBook-Pro-de-Benjamin-3.local>
2015-10-31 16:38:47 +0100

    single.php

--- single-recette.php
+++ single-recette.php
@@ -6,11 +6,13 @@
 </div>
 </div>
 
+<?php while(have_posts()) : the_post() ?>
+
 <div class="headerCategory">
     <div class="headerPic"></div>
     <div class="navbarCategory">
         <div class="container">
-        <h2>Soirées > Soirée Cocktail</h2>
+        <h2><?php the_terms($post->ID, 'type','');  ?> > <?php the_title(); ?> </h2>
         </div>
     </div>
     </div>
@@ -20,33 +22,42 @@
     <div class="singleLeft col-xs-12 col-md-9">
         <section class="singleEventItem">
             <div class="singleEventHeader">
-                <h2>soirée cocktail</h2>
+                <h2><?php the_title(); ?></h2>
             </div>
             <div class="singleEventContent">
                 <div class="singleEventThumbnail col-xs-12 col-md-3">
-                    <div class="bgimage"></div>
+                    <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
+                    <div class="bgimage" style="background-image:url(<?php echo $url ?>)"></div>
                 </div>
                 <div class="singleEventInfos col-xs-12 col-md-9">
                     <ul class="ingredients col-xs-6">
+
                         <span>Ingrédients :</span>
+                        <li><i class="fa fa-check"></i><?php $values = get_post_custom_values("ingredients"); echo $values[0]; ?></li>
+                        <!-- <li><i class="fa fa-check"></i>Ingrédient</li>
                         <li><i class="fa fa-check"></i>Ingrédient</li>
-                        <li><i class="fa fa-check"></i>Ingrédient</li>
-                        <li><i class="fa fa-check"></i>Ingrédient</li>
-                        <li><i class="fa fa-check"></i>Ingrédient</li>
+                        <li><i class="fa fa-check"></i>Ingrédient</li> -->
                     </ul>
                     <ul class="metadata col-xs-6">
-                        <li><span class="red">Postée le : </span>12/12/12</li>
-                        <li><span class="red">Préparation : </span>3h40</li>
-                        <li><span class="red">Pour : </span>4 à 6 personnes</li>
+                        <li><span class="red">Durée : </span><?php $values = get_post_custom_values("duree"); echo $values[0]; ?></li>
+                        <li><span class="red">Pour : </span><?php $values = get_post_custom_values("prix"); echo $values[0]; ?> personnes</li>
+                        <li><span class="red">Âge minimum : </span><?php $values = get_post_custom_values("age"); echo $values[0]; ?></li>
+                        <li><span class="red">Prix : </span><?php $values = get_post_custom_values("prix"); echo $values[0]; ?>/5</li>
+
                     </ul>
                 </div>
                 <div class="singleEventTxt col-xs-12">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere tenetur fugiat harum deserunt architecto adipisci est nihil doloremque, natus, recusandae cumque libero, commodi rem atque exercitationem perferendis! Iusto, accusantium, cum.</p>
+                    <p><?php the_content(); ?></p>
                 </div>
-            </div>   
-        <div class="clear"></div>
-        </section>
+            </div> 
+            <div class="clear"></div>
+            <div class="singleEventMetaInfos">
+                <span class="author">Postée par <?php the_author(); ?></span>
+                <span class="author">le <?php the_date(); ?></span>
+            </div>          </section>
     </div>
+
+<?php endwhile; ?>
 
    <section class="sidebar singleSidebar col-xs-12 col-md-3">
         <div class="searchbar">
