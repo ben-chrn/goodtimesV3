f933a7d12d5048bcfc10cad28af2dd869bd90ec0
Benjamin Chareyron <BenChrn@MacBook-Pro-de-Benjamin-3.local>
2015-11-04 12:33:51 +0100

    lissage homepage

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         
     <div class="container">
         <section class="moment">
-            <div class="row">
+            <h3 class="headerTitle">Quel moment voulez-vous passer en notre compagnie ?</h3>
             <?php $matinee = get_term_by('name', 'matinee', 'type') ?>
                 <?php $url = get_taxonomy_image($matinee->term_id,'home'); ?>
                 <article class="col-xs-12 col-sm-6 col-md-6 col-lg-4 day" style="background:url('<?php echo $url ?>'); background-size:cover; background-position:center top;">
@@ -57,60 +57,59 @@
                         </a>
                     </div>
                 </article>
-            </div>
-        </section>
+            </section>
         
-        <div class="clear"></div>
+            <div class="clear"></div>
 
-        <div class="content_home">
-            <div class="row">
-                <section class="col-md-6 col-lg-6 recipe">
-                      <h2>La recette du jour !</h2><br>
-                         <?php
-                            $args = array( 'post_type' => "recette", 'orderby' => 'rand', 'posts_per_page' => '1');
-                            
-                            $loop = new WP_Query( $args );
-                            while ( $loop->have_posts() ) : $loop->the_post(); ?>
+            <div class="content_home">
+                    <section class="col-xs-12 col-lg-6 recipe">
+                          <h2>La recette du jour !</h2><br>
+                             <?php
+                                $args = array( 'post_type' => "recette", 'orderby' => 'rand', 'posts_per_page' => '1');
+                                
+                                $loop = new WP_Query( $args );
+                                while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-                                <a href="<?php the_permalink();?>">
-                                <?php the_post_thumbnail(array(100,100) , array( 'class' => 'dayRecipee_thumbnail col-xs-12 col-md-3' ) ); ?>
-                                    <?php the_excerpt(); ?>
+                                    <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
+                                    <?php the_post_thumbnail(array(100,100) , array( 'class' => 'dayRecipee_thumbnail col-xs-12 col-md-3' ) ); ?>
+                                        <?php the_excerpt(); ?>
+                                    </a>
+                            <div class="clear"></div>
+                            <div class="moreread">
+                                <a href="<?php the_permalink(); ?>">
+                                    <p>Lire la suite</p>
                                 </a>
-                        <div class="clear"></div>
-                        <div class="moreread">
-                            <a href="<?php the_permalink(); ?>">
-                                <p>Lire la suite</p>
-                            </a>
-                        </div>
-                        <?php endwhile;
-                        wp_reset_postdata();
-                    ?>
-                </section>
-
-                <section class="col-md-5 col-lg-5 col-md-offset-1 col-lg-offset-1 more_consult">
-                     <h2>Les plus consultés</h2>
-                      <?php
-                       query_posts('meta_key=post_views_count&orderby=meta_value_num&order=DESC');
-                       if (have_posts()) : while (have_posts()) : the_post(); ?>
-                          <a href="<?php the_permalink(); ?>">
-                                <article>
-                                    <div class="left_picture">
-                                        <img src="http://pipsum.com/80x80.jpg"/>
-                                    </div>
-                                    <div class="right_text"> 
-                                        <h3><?php the_title(); ?></h3>
-                                        <p>
-                                            <?php the_excerpt(); ?>
-                                        </p>
-                                    </div>
-                               </article>
-                            </a>
-                            <?php
-                               endwhile; endif;
-                                wp_reset_query();
+                            </div>
+                            <?php endwhile;
+                                wp_reset_postdata();
                             ?>
-                        </section>
-                    </div>
+                    </section>
+
+                    <section class="col-xs-12 col-lg-6 more_consult">
+                         <h2>Les plus consultés</h2>
+                          <?php
+                           query_posts('meta_key=post_views_count&orderby=meta_value_num&order=DESC');
+                           if (have_posts()) : while (have_posts()) : the_post(); ?>
+                              <a href="<?php the_permalink(); ?>">
+                                    <article>
+                                        <div class="left_picture">
+                                            <img src="http://pipsum.com/80x80.jpg"/>
+                                        </div>
+                                        <div class="right_text"> 
+                                            <h3><?php the_title(); ?></h3>
+                                            <p>
+                                                <?php the_excerpt(); ?>
+                                            </p>
+                                        </div>
+                                   </article>
+                                </a>
+                                <?php
+                                   endwhile; endif;
+                                    wp_reset_query();
+                                ?>
+                    </section>
+                    <div class="clear"></div>
+                </div>
             </div>
         </div>
 </body>
